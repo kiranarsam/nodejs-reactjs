a985bb8028d78afc6866d20934a5b9553ab326fd
Kiran A <13688319+kiranarsam@users.noreply.github.com>
2023-08-30 20:58:20 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { FavoriteColor } from './hooks/hooks';
+import { TimerUseEffect } from './hooks/use-effect';
+import { ReactRefTrackingStateChanges, ReactUseRefUsage } from './hooks/use-ref';
+
+
 
 function App() {
   return (
     <div className="App">
       <h1> Hello World! </h1>
       <h3> Hello, I am using React. </h3>
+      <FavoriteColor></FavoriteColor>
+      <TimerUseEffect></TimerUseEffect>
+      <ReactUseRefUsage></ReactUseRefUsage>
+      <ReactRefTrackingStateChanges></ReactRefTrackingStateChanges>
     </div>
   );
 }
